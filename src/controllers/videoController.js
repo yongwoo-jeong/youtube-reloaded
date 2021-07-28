68e658e467eb46c1c40d11b28dd00e2c4336745a
yongwoo-jeong <jywoo9@gmail.com>
2021-07-28 16:35:19 +0900

    done mixin concept

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,32 @@
-const fakeUser = {
-  username: "Nicolas",
-  loggedIn: false,
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      view: 59,
+      di: 1,
+    },
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      view: 59,
+      di: 1,
+    },
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      view: 59,
+      di: 1,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
 };
-
-export const trending = (req, res) =>
-  res.render("home", { pageTitle: "Home", fakeUser });
 export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const search = (req, res) => res.send("Search");
