a038160ea467d71b4221ca9092afa01b441ff312
yongwoo-jeong <jywoo9@gmail.com>
2021-07-30 16:37:51 +0900

    seperate db from server js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,8 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
-
-const PORT = 4000;
 
 const app = express();
 
@@ -20,7 +16,4 @@ app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT} 🔥`);
-
-app.listen(4000, handleListening);
+export default app;
