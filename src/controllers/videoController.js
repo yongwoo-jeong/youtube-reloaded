76786aa1fa359d72bf4006c9aa543664662f8fb0
yongwoo-jeong <jywoo9@gmail.com>
2021-07-31 23:20:49 +0900

    async and await for db

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,10 @@
 import Video from "../models/Video";
-export const home = (req, res) => {
-  Video.find({}, (error, documents) => {});
-  res.render("home", { pageTitle: "Home" });
+
+export const home = async (req, res) => {
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
   const { id } = req.params;
   res.render("watch", { pageTitle: `Watching` });
